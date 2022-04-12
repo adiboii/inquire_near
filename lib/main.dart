2ab92f01da3513ce8339475a2b653899f80f908b
adi.boyii <adireyes00@gmail.com>
2022-04-12 14:20:29 +0800

    starting code

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,14 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-    //implement routes
+      home: Home(),
     ));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
