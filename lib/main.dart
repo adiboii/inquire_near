fbe9f4ab8328b390c357281dd68b6c9aaab59771
adi.boyii <adireyes00@gmail.com>
2022-05-04 22:50:17 +0800

    added routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:inquire_near/login.dart';
+import 'package:inquire_near/signup.dart';
+import 'package:inquire_near/home_page.dart';
+import 'package:inquire_near/themes/app_theme.dart';
 
-void main() => runApp(MaterialApp(
-      home: Home(),
-    ));
+void main() => runApp(InquireNear());
 
-class Home extends StatelessWidget {
-  const Home({Key? key}) : super(key: key);
+class InquireNear extends StatelessWidget {
+  const InquireNear({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: AppThemeData,
+      initialRoute: '/home_page',
+      routes: {
+        '/home_page': (context) => HomePage(),
+        '/login': (context) => LoginPage(),
+        '/sign_up': (context) => SignUpPage(),
+      },
+    );
   }
 }
