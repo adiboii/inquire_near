1544aa1fbf41f99c94b06985ce951cf014516d01
adi.boyii <adireyes00@gmail.com>
2022-05-10 21:53:48 +0800

    added dashboard route

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:inquire_near/dashboard.dart';
 import 'package:inquire_near/login.dart';
 import 'package:inquire_near/signup.dart';
 import 'package:inquire_near/home_page.dart';
 import 'package:inquire_near/themes/app_theme.dart';
 
-void main() => runApp(InquireNear());
+final FirebaseAuth _firebaseAuth = FirebaseAuth.instance;
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(InquireNear());
+}
 
 class InquireNear extends StatelessWidget {
   const InquireNear({Key? key}) : super(key: key);
@@ -19,6 +28,7 @@ class InquireNear extends StatelessWidget {
         '/home_page': (context) => HomePage(),
         '/login': (context) => LoginPage(),
         '/sign_up': (context) => SignUpPage(),
+        '/dashboard': (context) => Dashboard(),
       },
     );
   }
