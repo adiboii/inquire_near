214d497d3ea720b0c975ce051929122fe99dfaa4
adi.boyii <adrianpauln.reyes@gmail.com>
2022-06-29 20:56:09 +0800

    added card page and object

---
+++ lib/card_page.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:inquire_near/components/Card.dart';
+import 'package:inquire_near/components/custom_button.dart';
+import 'package:inquire_near/themes/app_color.dart';
+
+import 'components/list_item.dart';
+
+class CardPage extends StatefulWidget {
+  const CardPage({Key? key}) : super(key: key);
+
+  @override
+  State<CardPage> createState() => _CardPageState();
+}
+
+class _CardPageState extends State<CardPage> {
+  @override
+  Widget build(BuildContext context) {
+    final width = MediaQuery.of(context).size.width;
+    final height = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back_ios, color: Colors.black),
+          onPressed: () => Navigator.of(context).pop(),
+        ),
+        elevation: 0,
+        title: Text(
+          "Your Card",
+          style: Theme.of(context).textTheme.headline2,
+        ),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.fromLTRB(20, 10, 20, 0),
+        child: Column(
+          children: [
+            inCard(height: height, width: width),
+            SizedBox(height: 10),
+            Row(
+              children: [
+                ButtonFill(
+                  label: "Top-Up",
+                  width: 170,
+                  onTap: () {
+                    Navigator.pushNamed(context, '/top-up');
+                  },
+                ),
+                SizedBox(width: 10),
+                ButtonFill(
+                  label: "Block Card",
+                  width: 170,
+                  color: AppColor.gray,
+                  textColor: Colors.black,
+                )
+              ],
+            ),
+            SizedBox(height: 20),
+            Container(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Recent Transactions",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3
+                        ?.copyWith(fontSize: 15),
+                  ),
+                  SizedBox(height: 10),
+                  Center(
+                    child: Container(
+                      height: height * 0.45,
+                      child: ListView(
+                        scrollDirection: Axis.vertical,
+                        children: [
+                          inListItem(
+                              color: Colors.red,
+                              type: "Payment",
+                              amount: "- ₱25.00"),
+                          SizedBox(height: 10),
+                          inListItem(
+                              color: Colors.green,
+                              type: "Top-up",
+                              amount: "+ ₱150.00"),
+                          SizedBox(height: 10),
+                          inListItem(
+                              color: Colors.red,
+                              type: "Payment",
+                              amount: "- ₱25.00"),
+                          SizedBox(height: 10),
+                          inListItem(
+                              color: Colors.green,
+                              type: "Top-up",
+                              amount: "+ ₱150.00"),
+                          SizedBox(height: 10),
+                          inListItem(
+                              color: Colors.red,
+                              type: "Payment",
+                              amount: "- ₱25.00"),
+                          SizedBox(height: 10),
+                          inListItem(
+                              color: Colors.green,
+                              type: "Top-up",
+                              amount: "+ ₱150.00"),
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/components/Card.dart
@@ -0,0 +1,72 @@
+import 'package:flutter/material.dart';
+
+import '../themes/app_color.dart';
+
+class inCard extends StatelessWidget {
+  double width;
+  double height;
+
+  inCard({required this.height, required this.width});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width * 0.90,
+      height: height * 0.27,
+      padding: EdgeInsets.fromLTRB(15, 0, 15, 20),
+      decoration: BoxDecoration(
+        color: AppColor.primary,
+        borderRadius: BorderRadius.all(Radius.circular(20)),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Cymmer Maranga",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3
+                        ?.copyWith(color: Colors.white),
+                  ),
+                  Text(
+                    "•••• •••• •••• 2457",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3
+                        ?.copyWith(color: Colors.white, fontSize: 15),
+                  ),
+                ],
+              ),
+              Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    "Total Balance",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3
+                        ?.copyWith(color: Colors.white, fontSize: 12),
+                  ),
+                  Text(
+                    "₱455.20",
+                    style: Theme.of(context)
+                        .textTheme
+                        .headline3
+                        ?.copyWith(color: Colors.white, fontSize: 25),
+                  ),
+                ],
+              )
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
